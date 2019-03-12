3879792029bc5ded4196836e7e2c10d73c3e5d94
Adalyat Nazirov <aenazirov@gmail.com>
2019-03-13 00:12:07 +0300

    add CryptoTests to check

---
+++ Tests/Common/Securities/Cryptos/CryptoTests.cs
@@ -0,0 +1,46 @@
+﻿using NodaTime;
+using NUnit.Framework;
+using QuantConnect.Securities;
+using QuantConnect.Securities.Crypto;
+using System;
+
+namespace QuantConnect.Tests.Common.Securities.Cryptos
+{
+    [TestFixture]
+    public class CryptoTests
+    {
+        private static TimeKeeper TimeKeeper
+        {
+            get { return new TimeKeeper(DateTime.Now, new[] { TimeZones.NewYork }); }
+        }
+
+        [Test]
+        [TestCase("BTCUSD", "USD")]
+        public void ConstructorParseBaseCurrencyBySymbolProps(string ticker, string quote)
+        {
+            var securities = new SecurityManager(TimeKeeper);
+            var transactions = new SecurityTransactionManager(null, securities);
+            var portfolio = new SecurityPortfolioManager(securities, transactions);
+            if (portfolio.CashBook.ContainsKey(quote))
+            {
+                portfolio.CashBook[quote].SetAmount(1000);
+            }
+            else
+            {
+                portfolio.CashBook.Add(quote, 0, 1000);
+            }
+            var cash = portfolio.CashBook[quote];
+            var symbol = Symbol.Create(ticker, SecurityType.Crypto, Market.GDAX);
+
+            var crypto = new Crypto(
+                symbol,
+                SecurityExchangeHours.AlwaysOpen(DateTimeZone.Utc),
+                cash,
+                SymbolProperties.GetDefault(quote),
+                portfolio.CashBook
+            );
+
+            Assert.AreEqual(symbol.Value.Replace(quote, ""), crypto.BaseCurrencySymbol);
+        }
+    }
+}
